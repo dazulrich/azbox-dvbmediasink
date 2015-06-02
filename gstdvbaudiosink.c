479836566249f6b527e45d70d48b6fc9a84bc850
dazulrich <dazulrich@gmail.com>
2015-06-02 23:56:03 +0100

    fix

--- gstdvbaudiosink.c
+++ gstdvbaudiosink.c
@@ -183,7 +183,6 @@ static guint gst_dvbaudiosink_signals[LAST_SIGNAL] = { 0 };
 #define XRAW "audio/x-raw"
 #define PCMCAPS \
 		"audio/x-raw, " \
-		"format = (string) { " GST_AUDIO_NE(S32) ", " GST_AUDIO_NE(/S24) ", " GST_AUDIO_NE(S16) ", S8, " GST_AUDIO_NE(U32) ", " GST_AUDIO_NE(U24) ", " GST_AUDIO_NE(U16) ", U8 }, " \
 		"layout = (string) { interleaved, non-interleaved }, " \
 		"rate = (int) [ 1/, " MAX_PCM_RATE " ], " "channels = (int) [ 1, 2 ]; "
 #endif

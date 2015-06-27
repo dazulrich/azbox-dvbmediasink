a33bb7a258588c3abfafd62afa7b0739237b3ce7
dazulrich <dazulrich@gmail.com>
2015-06-27 03:51:31 +0100

    more

--- gstdvbaudiosink.c
+++ gstdvbaudiosink.c
@@ -133,26 +133,39 @@ static guint gst_dvbaudiosink_signals[LAST_SIGNAL] = { 0 };
 		"framed =(boolean) true; "
 
 #define WMACAPS \
-		"audio/x-wma; " \
+		"audio/x-wma, " \
+		"framed =(boolean) true; "
 
 #define AMRCAPS \
 		"audio/AMR, " \
 		"rate = (int) {8000, 16000}, channels = (int) 1; "
 
 #define XRAW "audio/x-raw"
-#if defined(DREAMBOX) || defined(MAX_PCMRATE_48K)
+
 #define PCMCAPS \
-		"audio/x-raw, " \
-		"format = (string) { "GST_AUDIO_NE(S32)", "GST_AUDIO_NE(S24)", "GST_AUDIO_NE(S16)", S8, "GST_AUDIO_NE(U32)", "GST_AUDIO_NE(U24)", "GST_AUDIO_NE(U16)", U8 }, " \
-		"layout = (string) { interleaved, non-interleaved }, " \
-		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ]; "
-#else
-#define PCMCAPS \
-		"audio/x-raw, " \
-		"format = (string) { "GST_AUDIO_NE(S32)", "GST_AUDIO_NE(S24)", "GST_AUDIO_NE(S16)", S8, "GST_AUDIO_NE(U32)", "GST_AUDIO_NE(U24)", "GST_AUDIO_NE(U16)", U8 }, " \
-		"layout = (string) { interleaved, non-interleaved }, " \
-		"rate = (int) [ 1, MAX ], " "channels = (int) [ 1, 2 ]; "
-#endif
+		"audio/x-raw-int, " \
+		"endianness = (int) { " G_STRINGIFY(G_BYTE_ORDER) " }, " \
+		"signed = (boolean) { TRUE, FALSE }, " \
+		"width = (int) 32, " \
+		"depth = (int) 32, " \
+		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ]; " \
+		"audio/x-raw-int, " \
+		"endianness = (int) { " G_STRINGIFY(G_BYTE_ORDER) " }, " \
+		"signed = (boolean) { TRUE, FALSE }, " \
+		"width = (int) 24, " \
+		"depth = (int) 24, " \
+		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ]; " \
+		"audio/x-raw-int, " \
+		"endianness = (int) { " G_STRINGIFY(G_BYTE_ORDER) " }, " \
+		"signed = (boolean) { TRUE, FALSE }, " \
+		"width = (int) 16, " \
+		"depth = (int) 16, " \
+		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ]; " \
+		"audio/x-raw-int, " \
+		"signed = (boolean) { TRUE, FALSE }, " \
+		"width = (int) 8, " \
+		"depth = (int) 8, " \
+		"rate = (int) [ 1, 48000 ], " "channels = (int) [ 1, 2 ];"
 
 static GstStaticPadTemplate sink_factory =
 GST_STATIC_PAD_TEMPLATE(
